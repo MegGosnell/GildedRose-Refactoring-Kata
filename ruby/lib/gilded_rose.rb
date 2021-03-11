a147627384adc745614e341121624c8a1a0d0f7b
megan gosnell <megan_gosnell@aol.co.uk>
2021-03-11 16:48:23 +0000

    decrease and increase methods created

--- ruby/lib/gilded_rose.rb
+++ ruby/lib/gilded_rose.rb
@@ -13,46 +13,46 @@ class GildedRose
   def update_item(item)
       if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
         if item.quality > 0 && item.name != "Sulfuras, Hand of Ragnaros"
-          item.quality = item.quality - 1
+          decrease_quality
         end
       else
-        if item.quality < 50
-          item.quality = item.quality + 1
+        increase_quality
           if item.name == "Backstage passes to a TAFKAL80ETC concert" && item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
+              increase_quality
+              if item.sell_in < 6 && item.quality < 50
+                item.quality += 1
               end
-            
-            if item.sell_in < 6 && item.quality < 50
-                item.quality = item.quality + 1
-            end
           end
-        
-      
-      if item.name != "Sulfuras, Hand of Ragnaros"
+        if item.name != "Sulfuras, Hand of Ragnaros"
         item.sell_in = item.sell_in - 1
-      end
+        end
       if item.sell_in < 0
         if item.name != "Aged Brie"
           if item.name != "Backstage passes to a TAFKAL80ETC concert"
             if item.quality > 0
               if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality = item.quality - 1
+                 item.quality -= 1
               end
             end
           else
             item.quality = item.quality - item.quality
           end
         else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
+          increase_quality
         end
       end
     end
   end
 end
+
+def increase_quality 
+  item.quality += 1 if quality < 50 
 end
+
+def decrease_quality 
+  item.quality -= 1 if quality > 0 
+end 
+
 
 class Item
   attr_accessor :name, :sell_in, :quality
