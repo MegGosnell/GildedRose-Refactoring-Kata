cce6bd3deb2224f73dff3824eb6c499bba528b4e
megan gosnell <megan_gosnell@aol.co.uk>
2021-03-11 15:43:51 +0000

    flattened some more if statements

--- ruby/lib/gilded_rose.rb
+++ ruby/lib/gilded_rose.rb
@@ -13,16 +13,13 @@ class GildedRose
       else
         if item.quality < 50
           item.quality = item.quality + 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
+          if item.name == "Backstage passes to a TAFKAL80ETC concert" && item.sell_in < 11
               if item.quality < 50
                 item.quality = item.quality + 1
               end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
+            
+            if item.sell_in < 6 && item.quality < 50
                 item.quality = item.quality + 1
-              end
             end
           end
         end
